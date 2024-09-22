3c87095b6008524566784aa006aadd3f7d039a80
serialfisher56 <112300122+noname20221@users.noreply.github.com>
2024-09-22 16:36:59 +0800

    remove empty interface revision 4

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -2,14 +2,9 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
-/* eslint-disable @typescript-eslint/no-empty-interface */
-export interface TextareaProps
-	extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
-/* eslint-enable @typescript-eslint/no-empty-interface */
-
 const Textarea = React.forwardRef<
 	HTMLTextAreaElement,
-	TextareaProps
+	React.TextareaHTMLAttributes<HTMLTextAreaElement>
 >(({ className, ...props }, ref) => {
 	return (
 		<textarea
